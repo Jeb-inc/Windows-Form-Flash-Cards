e7b46c8f065276b8d7694bec679fc943b873bccd
Chipley <59574696+Chipley@users.noreply.github.com>
2020-02-17 14:59:20 +0000

    Disable Sizing

--- Windows Form Flash Cards/Windows Form Flash Cards/Form1.Designer.cs
+++ Windows Form Flash Cards/Windows Form Flash Cards/Form1.Designer.cs
@@ -28,29 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.subjectSelector = new System.Windows.Forms.ComboBox();
+            this.reviseBt1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // subjectSelector
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Computer Science"});
-            this.comboBox1.Location = new System.Drawing.Point(7, 23);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(116, 21);
-            this.comboBox1.TabIndex = 0;
+            this.subjectSelector.FormattingEnabled = true;
+            this.subjectSelector.Items.AddRange(new object[] {
+            "Computer Science",
+            "English Literature",
+            "Product Design",
+            "Chemistry",
+            "Biology",
+            "Physics",
+            "German",
+            "History",
+            "Maths"});
+            this.subjectSelector.Location = new System.Drawing.Point(7, 23);
+            this.subjectSelector.Name = "subjectSelector";
+            this.subjectSelector.Size = new System.Drawing.Size(116, 21);
+            this.subjectSelector.TabIndex = 0;
             // 
-            // button1
+            // reviseBt1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.reviseBt1.Location = new System.Drawing.Point(129, 23);
+            this.reviseBt1.Name = "reviseBt1";
+            this.reviseBt1.Size = new System.Drawing.Size(75, 23);
+            this.reviseBt1.TabIndex = 1;
+            this.reviseBt1.Text = "Revise";
+            this.reviseBt1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -58,20 +66,23 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.Size = new System.Drawing.Size(116, 20);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Select Subjects:";
+            this.label1.Text = "Select Subject:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(210, 52);
+            this.ClientSize = new System.Drawing.Size(210, 49);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.reviseBt1);
+            this.Controls.Add(this.subjectSelector);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flashcards";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -80,8 +91,8 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox subjectSelector;
+        private System.Windows.Forms.Button reviseBt1;
         private System.Windows.Forms.Label label1;
     }
 }
